22e92140ce641f3723a3bf484828f0452c2ef33b
AryanShukla13 <101211852+AryanShukla13@users.noreply.github.com>
2023-11-24 23:48:21 +0530

    Add files via upload

---
+++ aryan_shukla.cpp
@@ -0,0 +1,66 @@
+<h1 align="center">Hi👋 I'm Aryan Shukla</h1>
+<!-- <p align="center"><a href="https://anuraghazra.github.io"><img width="80%" src="./assets/gh-readme-header.png" /></a></p> -->
+<h3 align="center">I am Aryan Shukla. I'm a currently working as a Programmer Analyst in Cognizant</h3>
+<br>
+<img align="right" alt="Coding" width="500" src="https://github.com/Ashish-0211/Ashish-0211/blob/main/ec8d4b3c350813be65b1711a528c5143.gif">
+<!-- <img align="right" alt="Coding" width="500" src="https://github.com/Ashish-0211/Ashish-0211/blob/main/git%20video.mp4"> -->
+
+- 🔭 I’m currently working on **Data Structures and Algorithms**
+
+- 🌱 I’m currently expanding my hands on **Competitive Programming, Salesforce and New Tecnologies**
+
+- 👯 I’m looking to collaborate on **Web Development**
+
+- 📫 How to reach me **aryanshukla1313@gmail.com**
+
+- ⚡ Fun fact **I think I am funny**!
+  
+
+<!-- <h3 align="left">Connect with me:</h3> -->
+<h3>🔍 Where to find me</h3>
+<p align="left">
+<a href="https://www.linkedin.com/in/aryanshukla1/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="ashish-kumar-sharma-8b9404217" height="30" width="40" /></a>
+<a href="https://instagram.com/ar_yan6724?igshid=NGVhN2U2NjQ0Yg==" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="ashish_a_k_s" height="30" width="40" /></a>
+// <a href="https://www.codechef.com/users/ashish_021120" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.1.0/icons/codechef.svg" alt="ashish_0211" height="30" width="40" /></a>
+// <a href="https://www.hackerrank.com/ashishaks0211" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/hackerrank.svg" alt="ashishaks0211" height="30" width="40" /></a>
+<a href="https://leetcode.com/Aryan_Shukla_/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="ashish0211" height="30" width="40" /></a>
+<a href="https://auth.geeksforgeeks.org/user/aryanshukla1313" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/geeks-for-geeks.svg" alt="ashishaks0211" height="30" width="40" /></a>
+//  <a href = "https://codeforces.com/profile/Ashish-0211" target = "blank"><img align = "center" alt="Codeforces" height = "20" width="140" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/1200px-Codeforces_logo.svg.png" /></a>
+</p>
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> </a> <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40" height="40"/> </a> <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> <a href="https://pugjs.org" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/pug.svg" alt="pug" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> </p>
+<br>
+<!-- <p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=ashish-0211&show_icons=true&locale=en&layout=compact" alt="ashish-0211" /></p> -->
+<p align="center">
+    <!-- <a href="https://github.com/SubhamRaoniar28/github-readme-streak-stats"> -->
+        <img title="🔥 Get streak stats for your profile at git.io/streak-stats" alt="Ashish's streak" src="https://github-readme-streak-stats.herokuapp.com/?user=Ashish-0211&theme=chartreuse-dark&hide_border=true&stroke=0000&background=060A0CD0"/>
+    </a>
+</p>
+## 📊 My Github Stats
+
+  <br/>
+  <p align = "center">
+    <!-- <a href="https://github.com/SubhamRaoniar28/github-readme-stats">--> <img alt="Ashish Kumar Sharma's Github Stats" src="https://github-readme-stats.vercel.app/api?username=Ashish-0211&show_icons=true&count_private=true&theme=react&hide_border=true&bg_color=0D1117" /></a>
+  <!-- <a href="https://github.com/SubhamRaoniar28/github-readme-stats"> --> <img alt="Ashish Kumar Sharma's Top Languages" src="https://github-readme-stats.vercel.app/api/top-langs/?username=Ashish-0211&langs_count=8&count_private=true&layout=compact&theme=react&hide_border=true&bg_color=0D1117" /></a>
+  </p>
+  <br/>
+  <b>Note:</b> Top languages is only a metric of the languages my public code consists of and doesn't reflect experience or skill level.
+
+
+<br/>
+<br/>
+
+<img alt="Ashish Kumar Sharma's Activity Graph" src="https://activity-graph.herokuapp.com/graph?username=Ashish-0211&bg_color=0D1117&color=5BCDEC&line=5BCDEC&point=FFFFFF&hide_border=true" />
+
+<br/>
+<br/>
+<!-- <br>
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=ashish-0211&show_icons=true&locale=en" alt="ashish-0211" /></p>
+<br>
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=ashish-0211&" alt="ashish-0211" /></p> -->
+<br/>
+
+
+<a href="https://github.com/Meghna-DAS/github-profile-views-counter">
+    <img src="https://komarev.com/ghpvc/?username=Ashish-0211">
+</a>
